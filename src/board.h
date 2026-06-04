ed931672fc0ed4e16d0d7661ba8e3144cf2e0f2d
mitchell-royan <mitchell.royan@my.jcu.edu.au>
2026-06-04 15:33:28 +1000

    Add accelerometer folder and files

--- src/board.h
+++ src/board.h
@@ -2,3 +2,12 @@
 
 #define LED_PIN 14
 #define NUM_LEDS 12
+#define I2C_SCL 17
+#define I2C_SDA 16
+#define ACCEL_I2C_ADDR 0x19 
+#define ACCEL_REG_WHO_AM_I 0x0F
+#define ACCEL_REG_CTRL1 0x20
+#define ACCEL_REG_CTRL4 0x23
+#define ACCEL_REG_OUT_X_L 0x28
+#define ACCEL_WHO_AM_I_VAL 0x33
+
